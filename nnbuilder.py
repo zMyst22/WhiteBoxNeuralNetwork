0982bc1ab6529cce9f05b0ec1f8d913b2e821045
Nicole <113491130+zMyst22@users.noreply.github.com>
2024-07-27 19:56:14 -0500

    NN Builder
    Slightly updated version of the original NN builder

--- nnbuilder.py
+++ nnbuilder.py
@@ -1,14 +1,13 @@
 import h5py
 import numpy as np
 
-def createNetwork(inputSize:int, hiddenLayers:tuple, outputSize:int, filename='default', weightRange=[-1,1], biasRange=[0]):
+def createNetwork(inputSize:int, hiddenLayers:tuple, outputSize:int, filename='default', weightRange=[0,1], biasRange=[0]):
     layers = [inputSize]+list(hiddenLayers)+[outputSize]
+    
     if len(biasRange) == 1:
-        biasMin = biasRange[0]
-        biasMax = biasRange[0]
+        biasMin, biasMax = biasRange[0], biasRange[0]
     else:
-        biasMin = biasRange[0]
-        biasMax = biasRange[1]
+        biasMin, biasMax = biasRange
 
     with h5py.File(f'{filename}.hdf5', 'w') as nn:
         wgrp = nn.create_group('weights')
@@ -17,15 +16,16 @@ def createNetwork(inputSize:int, hiddenLayers:tuple, outputSize:int, filename='d
             w = f'w{i+1}'
             b = f'b{i+1}'
             weight = np.random.uniform(weightRange[0],weightRange[1],(layers[i+1],layers[i]))
-            bias = np.random.uniform(biasMin, biasMax,(layers[i+1],))
-            dset = wgrp.create_dataset(w, data=weight)
-            dset = bgrp.create_dataset(b, data=bias)
+            bias = np.random.uniform(biasMin, biasMax,(layers[i+1],)).round(2)
+            wdset = wgrp.create_dataset(w, data=weight)
+            bdset = bgrp.create_dataset(b, data=bias)
         print(f'File saved as: {filename}.hdf5')
-        print(f'Network has {inputSize} input neurons, {len(hiddenLayers)} hidden layers, and {outputSize} output neurons.')
+        print(f'Network has {inputSize} input neuron(s), {len(hiddenLayers)} hidden layer(s), and {outputSize} output neuron(s).')
         print(f'The initial weight range is between {weightRange[0]} and {weightRange[1]}.')
         if len(biasRange)==1:
             print(f'Biases have been initialized at {biasRange[0]}.')
         else:
             print(f'Biases have been randomly initialized between {biasRange[0]} and {biasRange[1]}')
 
-createNetwork(5,(3,8,5),4)
+if __name__ == '__main__':
+    createNetwork(5, (4,3),2)
