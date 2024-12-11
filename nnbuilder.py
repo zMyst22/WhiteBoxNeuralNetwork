592d856e83f0398a355ad485e128775255f1d050
Nic <113491130+zMyst22@users.noreply.github.com>
2024-12-11 14:17:59 -0600

    Updated Secondary Files
    Updates to the secondary files and added in my training and testing data

--- nnbuilder.py
+++ nnbuilder.py
@@ -1,25 +1,26 @@
 import h5py
 import numpy as np
 
-def createNetwork(inputSize:int, hiddenLayers:tuple, outputSize:int, filename='default', weightRange=[0,1], biasRange=[0]):
-    layers = [inputSize]+list(hiddenLayers)+[outputSize]
+def createNetwork(inputSize:int, hiddenLayers:tuple, outputSize:int, filename='default.hdf5', weightRange=[-1,1], biasRange=[-1,1], precision=2):
+    if hiddenLayers == None:
+        hiddenLayers = []
+        layers =[inputSize]+[outputSize]
+    else:
+        layers = [inputSize]+list(hiddenLayers)+[outputSize]
     
     if len(biasRange) == 1:
         biasMin, biasMax = biasRange[0], biasRange[0]
     else:
         biasMin, biasMax = biasRange
 
-    with h5py.File(f'{filename}.hdf5', 'w') as nn:
-        wgrp = nn.create_group('weights')
-        bgrp = nn.create_group('biases')
-        for i in range(len(layers)-1):
-            w = f'w{i+1}'
-            b = f'b{i+1}'
-            weight = np.random.uniform(weightRange[0],weightRange[1],(layers[i+1],layers[i]))
-            bias = np.random.uniform(biasMin, biasMax,(layers[i+1],)).round(2)
-            wdset = wgrp.create_dataset(w, data=weight)
-            bdset = bgrp.create_dataset(b, data=bias)
-        print(f'File saved as: {filename}.hdf5')
+    with h5py.File(filename, 'w') as nn:
+        for layer in range(len(layers)-1):
+            weights = np.random.uniform(weightRange[0],weightRange[1],(layers[layer+1],layers[layer])).round(precision)
+            biases = np.random.uniform(biasMin, biasMax,(layers[layer+1],)).round(precision)
+            lgrp = nn.create_group(str(layer+1))
+            wgrp = lgrp.create_dataset('weights', data=weights)
+            bgrp = lgrp.create_dataset('biases', data=biases)
+        print(f'File saved as: {filename}')
         print(f'Network has {inputSize} input neuron(s), {len(hiddenLayers)} hidden layer(s), and {outputSize} output neuron(s).')
         print(f'The initial weight range is between {weightRange[0]} and {weightRange[1]}.')
         if len(biasRange)==1:
@@ -28,4 +29,4 @@ def createNetwork(inputSize:int, hiddenLayers:tuple, outputSize:int, filename='d
             print(f'Biases have been randomly initialized between {biasRange[0]} and {biasRange[1]}')
 
 if __name__ == '__main__':
-    createNetwork(5, (4,3),2)
+    createNetwork(784, None ,10, filename='test.hdf5')
--- nnrun.py
+++ nnrun.py
@@ -3,7 +3,6 @@ import csv
 import numpy as np
 import h5py as h5
 import time as t
-import zverter as z
 
 def activateLayer(input, weights, bias, activation=None):
     output=np.array([sum(input * weight) for weight in weights])
@@ -21,34 +20,37 @@ def sigmoid(input):
     output = 1/(1+np.exp(-input))
     return output
 
+def noAct(input):
+    return input
+
 def relu(input):
     return np.maximum(0,input)
 
 def readLayers(filename):
     info = {}
     with h5.File(filename, 'r') as nn:
-        weights = nn['weights']
-        biases = nn['biases']
-        wkeys = sorted(list(nn['weights'].keys()), key=lambda x: int(x[1:]))
-        bkeys = sorted(list(nn['biases'].keys()), key=lambda x: int(x[1:]))
-
-        for weight, bias in zip(wkeys, bkeys):
-            info[int(weight[1:])] = {'w':np.array(weights[weight]), 'b':np.array(biases[bias])}
+        lkeys = sorted(list(map(lambda x: int(x), list(nn.keys()))))
+        for lkey in lkeys:
+            info[int(lkey)] = {'weights': np.array(nn[str(lkey)]['weights']), 'biases' : np.array(nn[str(lkey)]['biases'])}
     return info
 
-def runNetwork(filename='default.hdf5', data=np.ndarray):
-    wb = readLayers(filename)
-    actScript = [softmax, relu, sigmoid]
+def runNetwork(filename='default.hdf5', data=np.ndarray, actScript=[None]):
+    if type(filename) == str:
+        wb = readLayers(filename)
+    else:
+        wb = filename
     for key, script in zip(wb.keys(), actScript):
-        data = activateLayer(data, wb[key]['w'], wb[key]['b'], script)
+        data = activateLayer(data, wb[key]['weights'], wb[key]['biases'], script)
 
-    print(np.argmax(data))
+    return np.argmax(data), max(data), data
 
-with open('mnist_train.csv','r') as f:
-    reader = csv.reader(f)
-    row = next(reader)
-row = list(map(int, row))
-image = (np.array(row[1:])/255).round(2)
+if __name__ == '__main__':
 
-nb.createNetwork(784, (32,16), 10, weightRange=[-1,1], biasRange=[-0.5,0.5])
-runNetwork(data=image)
+    with open('mnist_train.csv','r') as f:
+       reader = csv.reader(f)
+       row = next(reader)
+    row = list(map(int, row))
+    image = (np.array(row[1:])/255).round(2)
+
+    #nb.createNetwork(784, (64,32), 10, weightRange=[-1,1], biasRange=[-0.5,0.5])
+    #print(runNetwork(data=image,actScript=[softmax, relu, sigmoid]))
