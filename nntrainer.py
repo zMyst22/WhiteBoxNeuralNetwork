c820db4c5e514d834f6e8cfc7c9a32e507a90b28
Nic <113491130+zMyst22@users.noreply.github.com>
2025-01-10 15:31:32 -0600

    PROGRESS
    Implemented the new activation script system, condensed most functions into a master file and reconfigured nntrainer.py for it, and updated the default.hdf5 file with the new metadata/activation script system.

--- nntrainer.py
+++ nntrainer.py
@@ -1,8 +1,6 @@
-import h5py as h5
-import nnbuilder as nb
-import nnrun as nr
-import numpy as np
 import csv
+import numpy as np
+import wbnn as nn
 from time import time
 
 def forProp(input, weights, bias, activation=None):
@@ -13,26 +11,15 @@ def forProp(input, weights, bias, activation=None):
     else:
         return activation(output), output
 
-def calcLoss(target, outputLayer):
-    targets = np.zeros(len(outputLayer))
-    targets[target] = 1
-    return np.sum(np.subtract(outputLayer, targets)**2 )
-
-def dxSigmoid(input):
-    num = nr.sigmoid(input)
-    return num*(1-num)
-
-def dxRelu(input):
-    return np.where(np.array(input) > 0, 1, 0)
-
 def train(epochs=1000, lr=0.1, nnfile='default.hdf5', trainingData=any):
-    wb = nr.readLayers(nnfile)
+    wb = nn.readLayers(nnfile)
+    metadata = wb.pop('metadata')
+    scripts, dxScripts = nn.actHandler(metadata['activationScript'])
     totalLoss = 0
     for epoch in range(epochs):
         epochLoss = 0
         for data in trainingData:
             #Left to right
-            scripts = np.array([nr.relu, nr.sigmoid])
             networkActivations = {}
             networkUnactivated = {}
             networkActivations[0] = data[1:]
@@ -50,7 +37,7 @@ def train(epochs=1000, lr=0.1, nnfile='default.hdf5', trainingData=any):
                     wb[numLayers]['weights'][index]-= lr*grad*layerInputs[numLayers-1]
                     wb[numLayers]['biases'][index]-= lr*grad
                     if numLayers > 1:
-                        dxActivation = dxScripts[numLayers-1]
+                        dxActivation = dxScripts[numLayers-2]
                         if dxActivation != None:
                             newGradients = np.array(grad*np.multiply(dxActivation(preActInputs[numLayers-1]),wb[numLayers]['weights'][index]))
                         else:
@@ -61,46 +48,33 @@ def train(epochs=1000, lr=0.1, nnfile='default.hdf5', trainingData=any):
 
             numLayers = len(wb)
             #Left to right
-            dxScripts = [None, dxRelu, dxSigmoid]
-            outputActivation = dxScripts.pop(-1)
+            outputActivation = dxScripts[-1]
             actualOutput = networkActivations[numLayers]
             outputErrors = np.subtract(actualOutput, targetOutput)
             gradients = np.multiply(outputErrors, outputActivation(actualOutput))
             backProp(gradients, networkActivations, networkUnactivated, numLayers, dxScripts)
-            
-            epochLoss += calcLoss(int(data[0]), layerOutput)
+            epochLoss += nn.calcLoss(int(data[0]), layerOutput)
         print(f'Epoch {epoch+1} - Loss:', epochLoss/len(trainingData))
         totalLoss += epochLoss/len(trainingData)
         
     print('Average loss: ', totalLoss/epochs)
-    saveModel(wb, 'layer.hdf5')
-    
-def saveModel(model:dict, modelName='default.hdf5'):
-    with h5.File(modelName, 'w') as m:
-        for key in model.keys():
-            m.create_group(str(key))
-            wgrp = m[str(key)]
-            wgrp['weights'] = model[key]['weights']
-            wgrp['biases'] = model[key]['biases']
-        print(f'Model saved as {modelName}')
-    return
+    wb['metadata'] = metadata
+    nn.saveModel(wb, 'layer.hdf5')
 
-if __name__ == '__main__':
-    with open('short_mnist.csv','r') as f:
-        def setUp(row=np.ndarray):
-                row = np.array(row, dtype=float)
-                label = row[0]
-                row/=255
-                row[0] = label
-                return row  
-        
-        reader = csv.reader(f)
-        trainingData = list(map(setUp, reader))
-        trainingData = np.array(trainingData).round(2)
-
-    start = time()
-    #nb.createNetwork(784, None, 10, weightRange=[1,1], biasRange=[0], filename='test.hdf5')
+if __name__ == '__main__': 
     np.set_printoptions(precision=8, suppress=True, linewidth=200)
+    #nn.buildNetwork(784, (64,), 10, weightRange=[-0.5,0.5], biasRange=[0], activationScript=['relu', 'sigmoid'], filename='test.hdf5')
+    
+    trainingData = nn.readMnist('short_mnist.csv')
+    start = time()
     train(10, lr=0.01, trainingData=trainingData, nnfile='default.hdf5')
     end = time()
-    print(end-start, "Seconds")
+    print(f'Training time: {end-start} seconds')
+
+    start = time()
+    nn.testNetwork('layer.hdf5', 'short_mnist_test.csv')
+    end = time()
+    print(f'Testing time: {end-start} seconds')
+    
+
+    
---
+++ wbnn.py
@@ -0,0 +1,176 @@
+import numpy as np
+import h5py as h5
+import csv
+
+###Activation Funtions followed by their derivatives###
+
+def relu(input):
+    return np.maximum(0,input)
+
+def dxRelu(input):
+    return np.where(np.array(input) > 0, 1, 0)
+
+def sigmoid(input):
+    output = 1/(1+np.exp(-input))
+    return output 
+
+def dxSigmoid(input):
+    num = sigmoid(input)
+    return num*(1-num)
+
+###Common Functions###
+
+#Converts strings into function objects
+def actHandler(scripts:list):
+    #When adding new activation functions, define them above and add them to the dictionary below in the same format
+    #Keep the strings all lower case
+    scriptsDict = {'relu' : relu,
+                   'dxrelu' : dxRelu,
+                   'sigmoid' : sigmoid,
+                   'dxsigmoid' : dxSigmoid}
+    actScript = []
+    dxActScript = []
+    for script in scripts:
+        actScript.append(scriptsDict[script])
+        dxActScript.append(scriptsDict[f'dx{script}'])
+    return tuple(actScript), tuple(dxActScript)
+
+#Performs an entire layer's activation. Taking in an input, all of that layer's weights and biases then applies the activation function
+def activateLayer(input, weights, bias, activation=None):
+    output=np.array([sum(input * weight) for weight in weights])
+    output+=bias
+    if activation == None:
+        return output
+    else:
+        return activation(output)
+    
+#Initializes a network with randoms weights and automatically creates the correct number of layers, neurons, weights, and biases. 
+#Randomizing starting biases is not common practice, but the functionality is there to try, although it is recommended to keep at default.
+def buildNetwork(inputSize:int, hiddenLayers:tuple, outputSize:int, activationScript:list, filename='default.hdf5', weightRange=[-1,1], biasRange=[0], precision=2):
+    if hiddenLayers == None:
+        hiddenLayers = []
+        layers =[inputSize]+[outputSize]
+    else:
+        layers = [inputSize]+list(hiddenLayers)+[outputSize]
+    
+    if len(biasRange) == 1:
+        biasMin, biasMax = biasRange[0], biasRange[0]
+    else:
+        biasMin, biasMax = biasRange
+    #Sidenote - This code is just elegant af 
+    with h5.File(filename, 'w') as nn:
+        for layer in range(len(layers)-1):
+            weights = np.random.uniform(weightRange[0],weightRange[1],(layers[layer+1],layers[layer])).round(precision)
+            biases = np.random.uniform(biasMin, biasMax,(layers[layer+1],)).round(precision)
+            lgrp = nn.create_group(str(layer+1))
+            wset = lgrp.create_dataset('weights', data=weights)
+            bset = lgrp.create_dataset('biases', data=biases)
+        mgrp = nn.create_group('metadata')
+        aset = mgrp.create_dataset('activationScript', data=activationScript, shape=len(activationScript), dtype=h5.string_dtype())
+        print(f'File saved as: {filename}')
+        print(f'Network has {inputSize} input neuron(s), {len(hiddenLayers)} hidden layer(s), and {outputSize} output neuron(s).')
+        print(f'The initial weight range is between {weightRange[0]} and {weightRange[1]}.')
+        if len(biasRange)==1:
+            print(f'Biases have been initialized at {biasRange[0]}.')
+        else:
+            print(f'Biases have been randomly initialized between {biasRange[0]} and {biasRange[1]}')
+
+#Calculates the loss between the actual output and the expected output, on the scale of an entire layer
+def calcLoss(target, outputLayer):
+    targets = np.zeros(len(outputLayer))
+    targets[target] = 1
+    return np.sum(np.subtract(outputLayer, targets)**2 )
+
+#Takes a HDF5 file and converts the data into a dictionary of Numpy arrays
+def readLayers(filename):
+    info = {}
+    with h5.File(filename, 'r') as nn:
+        info['metadata'] = {'activationScript': [script.decode() for script in nn['metadata']['activationScript']]}
+        keys = list(nn.keys())
+        keys.remove('metadata')
+        lkeys = sorted(list(map(lambda x: int(x), keys)))
+        for lkey in lkeys:
+            info[int(lkey)] = {'weights': np.array(nn[str(lkey)]['weights']), 'biases' : np.array(nn[str(lkey)]['biases'])}
+    return info
+    
+#Converts MNIST CSV data into a format our network can use. Used for pulling the training data as well as the testing data
+def readMnist(csvfile):
+    with open(csvfile,'r') as f:
+        #setUp's only purpose is to format the data from the CSVs, therefore is childed here
+        def setUp(row=np.ndarray):
+                row = np.array(row, dtype=float)
+                label = row[0]
+                row/=255
+                row[0] = label
+                return row  
+        
+        reader = csv.reader(f)
+        dirtyData = list(map(setUp, reader))
+        cleanData = np.array(dirtyData).round(2)
+    return cleanData
+
+#Stitches together our readLayers and activateLayer functions. filename can either be an actual file name or can be a dictionary if readLayers was used previously
+def runNetwork(filename='default.hdf5', data=np.ndarray, actScript=None):
+    if type(filename) == str:
+        wb = readLayers(filename)
+    else:
+        wb = filename
+    if actScript == None:
+        actScript = actHandler(wb['metadata']['activationScript'])
+    keys = list(wb.keys())
+    keys.remove('metadata')
+    for key, script in zip(keys, actScript):
+        data = activateLayer(data, wb[key]['weights'], wb[key]['biases'], script)
+
+    return np.argmax(data), max(data), data
+
+#Saves the network into an HDF5 model, with model being the network and modelName being the file name to be saved under
+def saveModel(model:dict, modelName='default.hdf5'):
+    with h5.File(modelName, 'w') as m:
+        metadata = model['metadata']
+        activationScript = metadata['activationScript']
+        mgrp = m.create_group('metadata')
+        mgrp.create_dataset('activationScript', data=activationScript, shape=len(activationScript), dtype=h5.string_dtype())
+        keys = list(model.keys())
+        keys.remove('metadata')
+        for key in keys:
+            m.create_group(str(key))
+            wgrp = m[str(key)]
+            wgrp['weights'] = model[key]['weights']
+            wgrp['biases'] = model[key]['biases']
+        print(f'Model saved as {modelName}')
+    return
+
+#Applies our network against a testing dataset, it shouldn't include any identical specimens from our training set
+def testNetwork(model, testingData, actScript=None):
+    #If the variable testingData comes in as a file name string, readMnist will activate. If it was already previously converted into an array, it will pass
+    if type(testingData) == str:
+        testingData = readMnist(testingData)
+    else:
+        pass
+    #Same deal as the testingData, if model is passed as a string, it will be converted. If not, it will be reassigned to wb and continues
+    if type(model) == str:
+        wb = readLayers(model)
+    elif type(model) == dict:
+        wb = model
+    if actScript == None:
+        actScript = actHandler(wb['metadata']['activationScript'])[0]
+    right, wrong = 0, 0
+    loss = 0.0
+    for data in testingData:
+        target = int(data[0])
+        outputNum, prediction, outputLayer = runNetwork(filename=wb,data=data[1:], actScript=actScript)
+        loss += calcLoss(target, np.array(outputLayer))
+        if outputNum == target:
+             right +=1
+        else:
+             wrong +=1
+
+    print(f'Target Num: {int(data[0])} Predicted Num: {outputNum}, Prediction: {prediction}, Loss: {calcLoss(int(data[0]), outputLayer)}', '\n', outputLayer, '\n')
+    print(f'Right: {right} \nWrong: {wrong} \nPercentage: {100*right/len(testingData)}% \nLoss: {loss/len(testingData)}')
+    return
+
+if __name__ == '__main__':
+    np.set_printoptions(precision=8, suppress=True, linewidth=200)
+    #buildNetwork(784, (64,), 10, ['relu','relu','sigmoid'], 'test.hdf5')
+    testNetwork('layer.hdf5', 'short_mnist_test.csv')
